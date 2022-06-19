40f6230902bc5b807d435b29e3c17a02990622cb
sanEliasJr <santanaeliasjunior@gmail.com>
2022-06-18 23:32:13 -0300

    adicionando time

--- js/game.js
+++ js/game.js
@@ -1,5 +1,6 @@
-const luigi = document.querySelector(".luigi")
-const pipe = document.querySelector(".pipe")
+const luigi = document.querySelector(".luigi");
+const pipe = document.querySelector(".pipe");
+const time = document.querySelector(".score");
 
 const jump = () => {
 
@@ -13,26 +14,34 @@ const jump = () => {
 
 }
 
+let countScore = 0;
 
-const loop  = setInterval(() =>{
-    const pipePosition = pipe.offsetLeft;
-    const luigiPosition = +window.getComputedStyle(luigi).bottom.replace('px','');
+const timeGame  = setInterval(() => {
 
-    console.log(luigiPosition)
-    if(pipePosition <= 80 && pipePosition > 0 && luigiPosition <100){
-        pipe.style.animation = 'none';
-        pipe.style.left = `${pipePosition}px`;       
-        
-        luigi.style.animation = 'none';
-        luigi.style.bottom = `${luigiPosition}px`;  
+    time.innerHTML = `<p>${countScore++}</p>`
+    const loop  = setInterval(() =>{
+        const pipePosition = pipe.offsetLeft;
+        const luigiPosition = +window.getComputedStyle(luigi).bottom.replace('px','');
+    
+        console.log(luigiPosition)
+        if(pipePosition <= 80 && pipePosition > 0 && luigiPosition <100){
+            pipe.style.animation = 'none';
+            pipe.style.left = `${pipePosition}px`;       
+            
+            luigi.style.animation = 'none';
+            luigi.style.bottom = `${luigiPosition}px`;  
+    
+            luigi.src = './img/game-over.png'
+            luigi.style.width = '180px'
+            luigi.style.left = '-40px'
+    
+            clearInterval(loop)
+            clearInterval(timeGame)
+        }
+    },10)
 
-        luigi.src = './img/game-over.png'
-        luigi.style.width = '180px'
-        luigi.style.left = '-40px'
+},1000)
 
-        clearInterval(loop)
-    }
-},10)
 
 
 document.addEventListener('keydown', jump);  
